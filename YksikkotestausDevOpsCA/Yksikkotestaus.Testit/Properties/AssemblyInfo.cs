16c0013d14dbc878f15bf214efdb8f962140e318
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-07-04 15:17:02 +0300

    Tallennetut muutokset 4.6.2018.Kesken.

---
+++ YksikkotestausDevOpsCA/Yksikkotestaus.Testit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Yksikkotestaus.Testit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Yksikkotestaus.Testit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bfce53ef-0bcc-44a1-9f3e-bfab7ab3b0f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ YksikkotestausDevOpsCA/Yksikkotestaus.Testit/PääsiäinenTesti.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yksikkötestaus;
+
+namespace Yksikkotestaus.Testit
+{
+    [TestClass] //Attribuutti
+    public class PääsiäinenTesti
+    {
+        [TestMethod] //Attribuutti
+        public void VarmistaPääsiäissunnuntainLaskenta()
+        {
+            //throw new Exception("Pöö!");
+            //uusi instanssi objektista:
+            Pääsiäinen pääsiäinen = new Pääsiäinen();
+            DateTime sunnuntai = pääsiäinen.GetEasterSunday(2018);
+
+            //Vaihtoehtoinen tapa kirjoittaa testausta:
+            //! = negaatio operaattori
+            //if (!(sunnuntai.Date == new DateTime(2018, 4, 1)))
+            //{
+            //    throw new Exception("Pääsiäinen laskettu väärin!");
+            //}
+            Assert.AreEqual(new DateTime(2018, 4, 1), sunnuntai.Date); 
+        }
+    }
+}
